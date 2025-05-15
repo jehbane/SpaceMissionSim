0489fd9b2853fe76c921fe6719fe2f17ead062db
jehbane <83189322+jehbane@users.noreply.github.com>
2025-05-15 15:09:31 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 
-public class Main 
+import javax.swing.JFrame;
+
+public class Main extends Canvas
 {
+	public static final long serialVersionUID = 1L;
+	
 	public static final double G = 6.67e-11;
 	public static final double EARTH_GRAVITY = 9.81; // gravity measured in m/s^2
 	public static final double MARS_GRAVITY = 3.73;
@@ -37,6 +46,22 @@ public class Main
 	
 	public static void main(String[] args)
 	{
-		// TODO: some example code
+		Main main = new Main();
+		main.setMinimumSize(new Dimension(0, 0));
+		main.setMaximumSize(new Dimension(1000, 1000));
+		main.setPreferredSize(new Dimension(480, 360));
+		
+		JFrame frame = new JFrame("Space Sim");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.add(main, BorderLayout.CENTER);
+		frame.pack();
+		frame.setResizable(true);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		Graphics g = main.getGraphics();
+		g.setColor(Color.BLACK);
+		g.drawRect(50, 50, main.getWidth(), main.getHeight());
 	}
 }
