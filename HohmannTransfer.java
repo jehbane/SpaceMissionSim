88731e7d114e74376c5e59a7704ba9a5406d8121
Karol Grondelski <145499485+KarolMFG@users.noreply.github.com>
2025-05-19 11:01:20 -0400

    Create HohmannTransfer.java

---
+++ HohmannTransfer.java
@@ -0,0 +1,15 @@
+package orbit;
+
+public class HohmannTransfer
+{
+    
+    
+    private final String name;
+    
+    public HohmannTransfer(String name)
+    {
+        this.name = name;
+    }
+
+
+}
