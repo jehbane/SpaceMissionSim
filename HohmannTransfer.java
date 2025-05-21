423755b9250fc8adb12e11ab991e63a30d6c1eb3
Karol Grondelski <145499485+KarolMFG@users.noreply.github.com>
2025-05-21 10:58:23 -0400

    Update HohmannTransfer.java

--- HohmannTransfer.java
+++ HohmannTransfer.java
@@ -5,15 +5,15 @@ public class HohmannTransfer
 	public static final double G = 6.67e-11;
 	
 	public static final double EARTH_GRAVITY = 9.81; // gravity measured in m/s^2
-	public static final double EARTH_GRAVITATIONAL_PARAM = 3.986e10; // measured in km^3/s^2
+	public static final double EARTH_GRAVITATIONAL_PARAM = 3.986e5; // measured in km^3/s^2
 	public static final double EARTH_MASS = 0;
-	public static final double EARTH_RADIUS = 6371;
+	public static final double EARTH_RADIUS = 6371; // measured in km
 	public static final double EARTH_ORBITAL_RADIUS = 1.496e8;
 	
 	public static final double MARS_GRAVITY = 3.73;
-	public static final double MARS_GRAVITATIONAL_PARAM = 00000;
+	public static final double MARS_GRAVITATIONAL_PARAM = 4.282e4;
 	public static final double MARS_MASS = 0;
-	public static final double MARS_RADIUS = 0;
+	public static final double MARS_RADIUS = 3390; // measured in km
 	public static final double MARS_ORBITAL_RADIUS = 2.279e8;
 	
 	public static final double SUN_GRAVITATIONAL_PARAM = 1.327e11;
@@ -27,7 +27,7 @@ public class HohmannTransfer
 		this.lowMarsOrbit = lmo;
 	}
 	
-	public double getDeltaV()
+	public double getEscapeDeltaV()
 	{
 		double orbitalVelocity = Math.sqrt(EARTH_GRAVITATIONAL_PARAM / (EARTH_RADIUS + lowEarthOrbit)); // vehicle's orbital velocity around Earth
 		double earthOrbitalVelocity = Math.sqrt(SUN_GRAVITATIONAL_PARAM / EARTH_ORBITAL_RADIUS); // Earth's orbital velocity around the Sun
@@ -39,15 +39,20 @@ public class HohmannTransfer
 		return deltaV * 1000D; // convert to SI units
 	}
 	
-	public double getSecondDeltaV() // need to update function later
+	public double getInsertionDeltaV()
 	{
-		double orbitalVelocity = Math.sqrt(EARTH_GRAVITATIONAL_PARAM / (EARTH_RADIUS + lowEarthOrbit)); // vehicle's orbital velocity around Earth
-		double earthOrbitalVelocity = Math.sqrt(SUN_GRAVITATIONAL_PARAM / EARTH_ORBITAL_RADIUS); // Earth's orbital velocity around the Sun
+		double marsOrbitalVelocity = Math.sqrt(SUN_GRAVITATIONAL_PARAM / MARS_ORBITAL_RADIUS);
 		double semiMajorAxis = (EARTH_ORBITAL_RADIUS + MARS_ORBITAL_RADIUS) / 2;
-		double perihelionVelocity = Math.sqrt((SUN_GRAVITATIONAL_PARAM) * ((2 / EARTH_ORBITAL_RADIUS) - (1 / semiMajorAxis)));
-		double hyperbolicSpeed = perihelionVelocity - earthOrbitalVelocity;
-		double escapeVelocity = Math.sqrt(orbitalVelocity * orbitalVelocity + hyperbolicSpeed * hyperbolicSpeed);
-		double deltaV = escapeVelocity - orbitalVelocity;
-		return deltaV * 1000D; // convert to SI units
+		double arrivalVelocity = Math.sqrt((SUN_GRAVITATIONAL_PARAM) * ((2 / MARS_ORBITAL_RADIUS) - (1 / semiMajorAxis)));
+		double hyperbolicSpeed = Math.abs(arrivalVelocity - marsOrbitalVelocity);
+		double orbitalVelocity = Math.sqrt(MARS_GRAVITATIONAL_PARAM / (MARS_RADIUS + lowMarsOrbit));
+		double periapsisVelocity = Math.sqrt((hyperbolicSpeed * hyperbolicSpeed) + ((2 * MARS_GRAVITATIONAL_PARAM) / (MARS_RADIUS + lowMarsOrbit)));
+		double deltaV = periapsisVelocity - orbitalVelocity;
+		return deltaV * 1000D;
 	}
+
+	// gravity assists
+	// Oberth effect
+	// slingshots
+
 }
