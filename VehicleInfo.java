ee67e2455733c83a65bdf9abcd02f4802758d175
Karol Grondelski <145499485+KarolMFG@users.noreply.github.com>
2025-05-21 10:56:52 -0400

    Update VehicleInfo.java

--- VehicleInfo.java
+++ VehicleInfo.java
@@ -2,19 +2,26 @@ package vehicle;
 
 public class VehicleInfo 
 {
+	public final String name;
+
 	public final double fuelMass;
 	public final double vehicleMass;
-
-	public double totalMass;
+	public final double totalMass;
 	
-	public VehicleInfo(double fuelMass, double vehicleMass)
+	public VehicleInfo(String name, double fuelMass, double vehicleMass)
 	{
+		this.name = name;
 		this.fuelMass = fuelMass;
 		this.vehicleMass = vehicleMass;
 
 		totalMass = fuelMass + vehicleMass;
 	}
 
+	public String getVehicleName()
+	{
+		return name;
+	}
+	
 	public double getFuelMass()
 	{
 		return fuelMass;
